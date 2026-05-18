8a7c0f86adca60a2fdf7690aa451a2cf5dfd5060
joppt28-dev <joppt28@gmail.com>
2026-05-18 16:54:16 -0500

    Parte 5

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -11,9 +11,14 @@ contract Hospital280076 {
         bool estado;
     }
 
-    Paciente[]  public pacientes;
+    //Paciente[]  public pacientes;
+    mapping (uint => Paciente) public pacientes;
+
+    uint[] public idsPacientes;
 
     address public dirContrato = address(this);
+
+    uint256 cantidad;
 
     modifier consultadoPor() {
         console.log("Ejecutado por: 280076 - Josue Paulo Pulido Torres");
@@ -24,33 +29,40 @@ contract Hospital280076 {
     }
 
     function agregarElemento(uint _id, string memory _nombre_paciente, uint _edad, bool _estado) public consultadoPor {
-        for (uint i = 0; i < pacientes.length; i++) {
-            require(pacientes[i].id != _id, "Error: El ID del paciente ya se encuentra registrado.");
-        }
+        require(pacientes[_id].id == 0, "Error: El ID del paciente ya se encuentra registrado.");
         require(_edad > 10, "La edad debe ser mayor a 10");
-        pacientes.push(Paciente(_id, _nombre_paciente, _edad, _estado));
+        pacientes[_id]= Paciente(_id, _nombre_paciente, _edad, _estado);
+        idsPacientes.push(_id);
+        cantidad ++;
     }
 
     function contarElementos () public view consultadoPor returns (Paciente[] memory) {
-        return pacientes;
+        Paciente[] memory p = new Paciente[](cantidad);
+        for (uint i = 0; i < idsPacientes.length; i++) {
+            uint idActual = idsPacientes[i];
+            p[i] = pacientes[idActual];
+        }
+        return p;
     }
 
-    function inactivarElemento (uint _posicion) public {
-        pacientes[_posicion].estado = false;
+    function inactivarElemento (uint _id) public {
+        pacientes[_id].estado = false;
     }
 
     function mostrarElementosActivos() public consultadoPor{
-        for (uint i = 0; i < pacientes.length; i++) {
-            if (pacientes[i].estado == true) {
-                console.log("Paciente activo : ID:", pacientes[i].id, "| Nombre:", pacientes[i].nombre_paciente);
+        for (uint i = 0; i < idsPacientes.length; i++) {
+            uint idActual = idsPacientes[i];
+            if (pacientes[idActual].estado == true) {
+                console.log("Paciente activo : ID:", pacientes[idActual].id, "| Nombre:", pacientes[idActual].nombre_paciente);
             }
         }
     }
 
     function mostrarElementosImpares() public consultadoPor {
-        for (uint i = 0; i < pacientes.length; i++) {
-            if (pacientes[i].id % 2 != 0) {
-                console.log("Paciente ID Impar : ID:", pacientes[i].id, "| Nombre:", pacientes[i].nombre_paciente);
+        for (uint i = 0; i < idsPacientes.length; i++) {
+            uint idActual = idsPacientes[i];
+            if (idActual % 2 != 0) {
+                console.log("Paciente ID Impar : ID:", pacientes[idActual].id, "| Nombre:", pacientes[idActual].nombre_paciente);
             }
         }
     }
