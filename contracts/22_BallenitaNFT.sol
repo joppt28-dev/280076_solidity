deb6033bbaa19184b5e48d67e9e86701b178fe92
joppt28-dev <joppt28@gmail.com>
2026-06-20 21:26:15 -0500

    carga 9

--- contracts/22_BallenitaNFT.sol
+++ contracts/22_BallenitaNFT.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BallenitaNFT is ERC721, Ownable {
 
+contract BallenitaNFT is ERC721, Ownable {
     uint256 private _contadorId = 1;
 
-    constructor()
+    constructor() 
         ERC721("Ballenita Poster Token - NFT", "BPT")
         Ownable(msg.sender)
     {
@@ -20,31 +20,52 @@ contract BallenitaNFT is ERC721, Ownable {
         string nombre;
         uint256 level;
         uint256 rareza;
+        uint256 precio;
     }
 
     mapping(uint256 => Poster) private posters;
 
-    function mint(address to, string memory _nombre, uint256 _level, uint256 _rareza) public onlyOwner {
-        
-        uint256 idPoster = _contadorId;
+    function mint(string memory _nombre, uint256 _level, uint256 _rareza) public onlyOwner {
 
-        /*posters[idPoster] = Poster({
-            id : _contadorId,
-            nombre : _nombre,
-            level : _level,
-            rareza : _rareza,
-        });*/
+        uint idPoster = _contadorId;
 
-        posters[idPoster] = Poster(idPoster, _nombre, _level, _rareza);
+        uint256 precioCalculado = 0.001 ether * _level * _rareza;
 
-        _safeMint(to, _contadorId);
-
+        posters[idPoster] = Poster(idPoster, _nombre, _level, _rareza, precioCalculado);
+        _mint(address(this), _contadorId);
         _contadorId++;
+
     }
 
-    function verPoster(uint256 tokenId) public view returns (uint256, string memory, uint256, uint256) {
+    function verPoster(uint256 tokenId) public view returns (uint256 id, string memory nombre, uint256 level, uint256 rareza, uint256 precio) {
         Poster memory poster = posters[tokenId];
-        return (poster.id, poster.nombre, poster.level, poster.rareza);
+        return (poster.id, poster.nombre, poster.level, poster.rareza, poster.precio);
     }
 
+    function verPostersProp(address propietario) public view returns(Poster[] memory){
+        uint256 cantidadPosters = balanceOf(propietario);
+        Poster[] memory resultado = new Poster[](cantidadPosters);
+        
+        uint256 contador = 0;
+
+        for(uint256 tokenId = 1; tokenId < _contadorId; tokenId++){
+            if(_ownerOf(tokenId) == propietario) {
+                resultado[contador] = posters[tokenId];
+                contador++;
+            }
+        }
+
+        return resultado;
+    }
+
+    function comprarPoster(uint256 tokenId) public payable {
+        require(ownerOf(tokenId) == address(this), "El poster no esta disponible");
+        Poster memory poster = posters[tokenId];
+        require(msg.value >= poster.precio, "ETH insuficiente");        
+        _transfer(address(this), msg.sender, tokenId);
+    }
+
+    function retirar () public onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }    
 }
